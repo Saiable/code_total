baafd50feb7dfb1ecae243851e60e810969fd0fc
Administrator <2495620791@qq.com>
2021-01-08 21:04:14 +0800

    注册页面

--- web/views/account.py
+++ web/views/account.py
@@ -2,3 +2,8 @@
 '''
 用户账户相关的功能：注册、短信、登陆、注销
 '''
+from django.shortcuts import render
+from web.forms.account import RegisterModelForm
+def register(request):
+    form = RegisterModelForm()
+    return render(request, 'register.html', {'form': form})
